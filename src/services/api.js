970fb06d8e5ada2e9f91f7611e697f24096a6a19
GspaceDevelopers <gpsacedevelopers@gmail.com>
2023-02-15 07:23:10 -0300

    usando server da raiway enquanto heroku nao volta

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const api = axios.create({
-  //baseURL: "https://apivittoriateste.up.railway.app/",
-  baseURL: "https://apivittoriaviitstore.herokuapp.com/",
+  baseURL: "https://web-production-a6075.up.railway.app/",
+ // baseURL: "https://apivittoriaviitstore.herokuapp.com/",
 });
 
 export default api;

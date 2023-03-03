918f7cfad3e3d76beef962ea6bb70f094c97b5bb
GspaceDevelopers <gpsacedevelopers@gmail.com>
2023-03-03 09:45:19 -0300

    09:45

--- src/Components/Carrossel/index.js
+++ src/Components/Carrossel/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import api2 from "../../services/api2";
 import "./Styles.css";
-
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
 
 const Carrossel = () => {
@@ -11,93 +12,97 @@ const Carrossel = () => {
     api2.get("/edicao").then((data) => {
       setDadosedicao(data.data);
     });
-
-    dadosedicao.map((item) => {
-      if (window.screen.width >= 500) {
-        document
-          .querySelector(".background-image-main-one")
-          .setAttribute(
-            "style",
-            `background-image:url(${item.componentelogos.logo1})`
-          );
-        document
-          .querySelector(".background-image-main-two")
-          .setAttribute(
-            "style",
-            `background-image:url(${item.componentelogos.logo2})`
-          );
-        document
-          .querySelector(".background-image-main-three")
-          .setAttribute(
-            "style",
-            `background-image:url(${item.componentelogos.logo3})`
-          );
-        document
-          .querySelector(".background-image-main-four")
-          .setAttribute(
-            "style",
-            `background-image:url(${item.componentelogos.logo4})`
-          );
-
-        document
-          .querySelector(".background-image-main-five")
-          .setAttribute(
-            "style",
-            `background-image:url(${item.componentelogos.logo5})`
-          );
-        document
-          .querySelector(".background-image-main-six")
-          .setAttribute(
-            "style",
-            `background-image:url(${item.componentelogos.logo6})`
-          );
-
-      } else {
-        document
-          .querySelector(".background-image-main-one")
-          .setAttribute(
-            "style",
-            `background-image:url(${item.banners.banner1})`
-          );
-        document
-          .querySelector(".background-image-main-two")
-          .setAttribute(
-            "style",
-            `background-image:url(${item.banners.banner2})`
-          );
-        document
-          .querySelector(".background-image-main-three")
-          .setAttribute(
-            "style",
-            `background-image:url(${item.banners.banner3})`
-          );
-        document
-          .querySelector(".background-image-main-four")
-          .setAttribute(
-            "style",
-            `background-image:url(${item.banners.banner4})`
-          );
-        document
-          .querySelector(".background-image-main-five")
-          .setAttribute(
-            "style",
-            `background-image:url(${item.banners.banner5})`
-          );
-        document
-          .querySelector(".background-image-main-six")
-          .setAttribute(
-            "style",
-            `background-image:url(${item.banners.banner6})`
-          );
-
-      }
-    });
   }, [dadosedicao]);
+
+  const slideImages = [
+    {
+      url: dadosedicao.map(item => item.componentelogos.logo1),
+      link: dadosedicao.map(item => item.url1)
+    },
+    {
+      url: dadosedicao.map(item => item.componentelogos.logo2),
+      link: dadosedicao.map(item => item.url2)
+
+    },
+    {
+      url: dadosedicao.map(item => item.componentelogos.logo3),
+      link: dadosedicao.map(item => item.url3)
+    },
+    {
+      url: dadosedicao.map(item => item.componentelogos.logo4),
+      link: dadosedicao.map(item => item.url4)
+    },
+    {
+      url: dadosedicao.map(item => item.componentelogos.logo5),
+      link: dadosedicao.map(item => item.url5)
+    },
+    {
+      url: dadosedicao.map(item => item.componentelogos.logo6),
+      link: dadosedicao.map(item => item.url6)
+    },
+
+  ];
+
+  const slideImages2 = [
+    {
+      url: dadosedicao.map(item => item.banners.banner1),
+      link: dadosedicao.map(item => item.url1)
+
+    },
+    {
+      url: dadosedicao.map(item => item.banners.banner2),
+      link: dadosedicao.map(item => item.url1)
+
+    },
+    {
+      url: dadosedicao.map(item => item.banners.banner3),
+      link: dadosedicao.map(item => item.url1)
+
+    },
+    {
+      url: dadosedicao.map(item => item.banners.banner4),
+      link: dadosedicao.map(item => item.url1)
+
+    },
+    {
+      url: dadosedicao.map(item => item.banners.banner5),
+      link: dadosedicao.map(item => item.url1)
+
+    },
+    {
+      url: dadosedicao.map(item => item.banners.banner6),
+      link: dadosedicao.map(item => item.url1)
+
+    },
+
+  ];
+
+  const filter = slideImages.filter(item => item.url != '')
+  const filter2 = slideImages2.filter(item => item.url != '')
 
   return (
 
-    <div>
+    <div className="container-pai-carrossel">
+      <div className="slide-container">
+        {window.screen.width > 500 ?
+          <Fade autoplay duration={3000} >
+            {filter.map((fadeImage, index) => (
+              <div key={index}>
+                <img onClick={()=> window.location.href=`${fadeImage.link}`} style={{ width: '100%' }} src={fadeImage.url} />
+              </div>
+            ))}
+          </Fade> :
 
+          <Fade autoplay duration={3000}>
+            {filter2.map((fadeImage, index) => (
+              <div key={index}>
+                <img onClick={()=> window.location.href=`${fadeImage.link}`} style={{ width: '100%' }} src={fadeImage.url} />
+              </div>
+            ))}
+          </Fade>
+
+        }
+      </div>
     </div>
 
 
@@ -107,7 +112,7 @@ const Carrossel = () => {
 
 
 
-);
+  );
 };
 
 export default Carrossel;
